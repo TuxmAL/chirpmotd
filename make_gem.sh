45958feadc57d9ab8c57c338f5b41125500ef17b
Antonio Liccardo <tuxmal@tiscali.it>
2017-08-31 19:11:57 +0200

    Modified script for gem creation, now it create the log dir and the idx file -- need root permission.

--- make_gem.sh
+++ make_gem.sh
@@ -1,2 +1,9 @@
-build.sh build chirpmotd.gemspec
-build.sh install --local --user-install chirp_motd-0.1.0.gem
+gem build chirpmotd.gemspec
+gem install --local --user-install chirp_motd-0.1.1.gem
+echo finalizing installation
+cp config/config.json.last /home/tony/.gem/ruby/2.4.0/gems/chirp_motd-0.1.1/config/config.json
+echo creating log file
+su -c "echo >/var/log/chirpmotd.log && chmod a+rw /var/log/chirpmotd.log"
+echo creating persisting dir
+su -c "mkdir /var/lib/chirpmotd && chmod a+rwx /var/lib/chirpmotd"
+echo done.
