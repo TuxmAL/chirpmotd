0640632d883934ad33b7a734bc3eca2aa6f24043
Antonio Liccardo <tuxmal@tiscali.it>
2017-11-12 21:06:31 +0100

    Little fix for config file.

--- make_gem.sh
+++ make_gem.sh
@@ -2,7 +2,7 @@ gem build chirpmotd.gemspec
 gem uninstall -v "0.1.2" chirp_motd
 gem install --local --user-install chirp_motd-0.1.3.gem
 echo finalizing installation
-cp config/config.json.last /home/tony/.gem/ruby/2.4.0/gems/chirp_motd-0.1.2/config/config.json
+cp config/config.json.good /home/tony/.gem/ruby/2.4.0/gems/chirp_motd-0.1.3/config/config.json
 echo creating log file
 su -c "echo >/var/log/chirpmotd.log && chmod a+rw /var/log/chirpmotd.log"
 echo creating persisting dir
