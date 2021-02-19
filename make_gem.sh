eaa2d42894dcdff5a79886b8c19c46f0b866e238
Antonio Liccardo <antonio.liccardo@citelgroup.it>
2021-02-19 18:15:12 +0100

    Updated script to make gem.

--- make_gem.sh
+++ make_gem.sh
@@ -1,8 +1,10 @@
 gem build chirpmotd.gemspec
-gem uninstall -v "0.1.3" chirp_motd
-gem install --local --user-install chirp_motd-0.1.4.gem
+gem uninstall chirp_motd
+#PATH="$(ruby -e 'print Gem.user_dir')/bin:$PATH"
+
+gem install --local --user-install chirp_motd-0.1.5.gem
 echo finalizing installation
-cp config/config.json.good /home/tony/.gem/ruby/2.4.0/gems/chirp_motd-0.1.3/config/config.json
+# cp config/config.json.good /home/tony/.gem/ruby/2.4.0/gems/chirp_motd-0.1.3/config/config.json
 echo creating log file
 su -c "echo >/var/log/chirpmotd.log && chmod a+rw /var/log/chirpmotd.log"
 echo creating persisting dir
