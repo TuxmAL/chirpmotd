23c3d5599fcd8c30085fe79cc24dab7c4d0ce96c
Antonio Liccardo <antonio.liccardo@citelgroup.it>
2021-03-21 19:31:25 +0100

    Doubled stories generation. Added an English version to the Italian one.

--- lib/modules/storie_gen.rb
+++ lib/modules/storie_gen.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 module ChirpQuote
-  class StoryGenerator
+  class StorieGenerator
     def initialize(config, logger)
       @logger = logger
       @uri = config['service_uri'].nil? ? '' : config['service_uri']
-      @uri = URI.join(@uri, 'tell')
+      @uri = URI.join(@uri, 'racconta')
       rescue => e
         @logger.error e.message
         @logger.error e.backtrace
@@ -15,7 +15,7 @@ module ChirpQuote
       body_resp = Net::HTTP.get @uri
       story = JSON.parse body_resp
       @logger.info "#{self.class}: #{story}"
-      "#ITTales #{story['value']}"
+      "#FoleInformatiche #{story['value']}"
     rescue StandardError => e
       @logger.error e.message
       @logger.error e.backtrace
