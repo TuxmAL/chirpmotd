31a3fe8c8a1f9b90b8ec580c038ee78b5578951d
Antonio Liccardo <antonio.liccardo@citelgroup.it>
2021-03-25 13:20:07 +0100

    Corrected a typo.

--- lib/modules/it_s_me.rb
+++ lib/modules/it_s_me.rb
@@ -9,14 +9,14 @@ module ChirpQuote
 
     def get
       begin
-        @logger.info "#{self.class}: advertising RaspyTux status to @TuxmAL"
+        @logger.info "#{self.class}: advertising RasPiTux status to @TuxmAL"
         text = ''
         MQTT::Client.connect(@uri) do |c|
           _, time = c.get '/tuxmalrpi/temp/time'
           _, gpu_temp = c.get '/tuxmalrpi/temp/gpu'
           _, cpu_temp = c.get '/tuxmalrpi/temp/cpu'
           _, throttle = c.get '/tuxmalrpi/temp/trottle_decoded'
-          text = "@TuxmAL, here RaspyTux: my CPU #{cpu_temp}, my GPU #{gpu_temp} at #{time}.\n#{throttle.gsub!('\\n',"\n").chomp!}"
+          text = "@TuxmAL, here RasPiTux: my CPU #{cpu_temp}, my GPU #{gpu_temp} at #{time}.\n#{throttle.gsub!('\\n',"\n").chomp!}"
         end
         @logger.info "#{self.class}: #{text}"
         return text
